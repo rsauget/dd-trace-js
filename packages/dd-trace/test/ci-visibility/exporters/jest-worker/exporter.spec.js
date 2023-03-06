559e503dccfbc4a4a58d2a42838de9a049f5b053
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2023-03-06 17:41:17 +0100

    fix unit test

--- packages/dd-trace/test/ci-visibility/exporters/jest-worker/exporter.spec.js
+++ packages/dd-trace/test/ci-visibility/exporters/jest-worker/exporter.spec.js
@@ -17,17 +17,23 @@ describe('CI Visibility Jest Worker Exporter', () => {
   })
   it('can export traces', () => {
     const trace = [{ type: 'test' }]
+    const traceSecond = [{ type: 'test', name: 'other' }]
     const jestWorkerExporter = new JestWorkerCiVisibilityExporter()
     jestWorkerExporter.export(trace)
+    jestWorkerExporter.export(traceSecond)
     jestWorkerExporter.flush()
-    expect(send).to.have.been.calledWith([JEST_WORKER_TRACE_PAYLOAD_CODE, JSON.stringify(trace)])
+    expect(send).to.have.been.calledWith([JEST_WORKER_TRACE_PAYLOAD_CODE, JSON.stringify([trace, traceSecond])])
   })
   it('can export coverages', () => {
     const coverage = { traceId: '1', spanId: '1', files: ['test.js'] }
+    const coverageSecond = { traceId: '2', spanId: '2', files: ['test2.js'] }
     const jestWorkerExporter = new JestWorkerCiVisibilityExporter()
     jestWorkerExporter.exportCoverage(coverage)
+    jestWorkerExporter.exportCoverage(coverageSecond)
     jestWorkerExporter.flush()
-    expect(send).to.have.been.calledWith([JEST_WORKER_COVERAGE_PAYLOAD_CODE, JSON.stringify([coverage])])
+    expect(send).to.have.been.calledWith(
+      [JEST_WORKER_COVERAGE_PAYLOAD_CODE, JSON.stringify([coverage, coverageSecond])]
+    )
   })
   it('does not break if process.send is undefined', () => {
     delete process.send
