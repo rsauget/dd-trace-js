27d39bd7f34c5511c5ff48945ad07b5f989be14e
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2023-03-31 14:58:56 -0400

    Crash investigation

--- integration-tests/profiler/oom.js
+++ integration-tests/profiler/oom.js
@@ -1,37 +1,4 @@
 'use strict'
 
 require('dd-trace').init()
-console.log('Starting oom')
-
-const { Worker, isMainThread } = require('worker_threads')
-
-if (isMainThread) {
-  const nworkers = Number(process.argv[2])
-  const workers = []
-  if (nworkers) {
-    for (let i = 0; i < nworkers; i++) {
-      workers.push(new Worker(__filename))
-    }
-  }
-}
-
-const leak = []
-let count = 0
-
-function foo (size) {
-  count += 1
-  console.log(`count: ${count}`)
-  const n = size / 8
-  const x = []
-  x.length = n
-  for (let i = 0; i < n; i++) { x[i] = Math.random() }
-  leak.push(x)
-
-  if (count < maxCount) { setTimeout(() => foo(size), sleepMs) }
-}
-
-const maxCount = process.argv[3] || 1
-const sleepMs = process.argv[4] || 50
-
-setTimeout(() => foo(5 * 1024 * 1024), sleepMs)
-console.log(`oom4 ${sleepMs}`)
+setTimeout(() => {}, 50)
