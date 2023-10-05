bf656a9539f37cd10266f281c327458279039abb
Sam Brenner <sam.brenner@datadoghq.com>
2023-10-05 15:25:16 -0400

    next changes

--- packages/datadog-plugin-next/test/next.config.js
+++ packages/datadog-plugin-next/test/next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  output: 'standalone',
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.NormalModuleReplacementPlugin(/^node:/, resource => {
-        resource.request = resource.request.replace(/^node:/, '')
-      })
-    )
-    config.resolve.preferRelative = true
-    return config
-  }
+  output: 'standalone'
 }
